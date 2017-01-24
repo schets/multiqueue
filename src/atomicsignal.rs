0ef76e6d48a627216807d4e7da6b6ac51d088829
Sam Schetterer <samschet@gmail.com>
2017-01-24 13:19:00 -0800

    Added needed inline flags

--- src/atomicsignal.rs
+++ src/atomicsignal.rs
@@ -18,45 +18,54 @@ impl AtomicSignal {
         AtomicSignal { flags: AtomicUsize::new(0) }
     }
 
+    #[inline(always)]
     pub fn load(&self, ord: Ordering) -> LoadedSignal {
         LoadedSignal { flags: self.flags.load(ord) }
     }
 
+    #[inline(always)]
     pub fn set_epoch(&self, ord: Ordering) -> bool {
         let prev = self.flags.fetch_or(UPDATE_EPOCH, ord);
         (prev & UPDATE_EPOCH) != 0
     }
 
+    #[inline(always)]
     pub fn clear_epoch(&self, ord: Ordering) -> bool {
         let prev = self.flags.fetch_and(!UPDATE_EPOCH, ord);
         (prev & UPDATE_EPOCH) != 0
     }
 
+    #[inline(always)]
     pub fn set_reader(&self, ord: Ordering) -> bool {
         let prev = self.flags.fetch_or(NO_READER, ord);
         (prev & NO_READER) != 0
     }
 
+    #[inline(always)]
     pub fn clear_reader(&self, ord: Ordering) -> bool {
         let prev = self.flags.fetch_and(!NO_READER, ord);
         (prev & NO_READER) != 0
     }
 
+    #[inline(always)]
     pub fn set_writer(&self, ord: Ordering) -> bool {
         let prev = self.flags.fetch_or(NO_WRITER, ord);
         (prev & NO_WRITER) != 0
     }
 
+    #[inline(always)]
     pub fn clear_writer(&self, ord: Ordering) -> bool {
         let prev = self.flags.fetch_and(!NO_READER, ord);
         (prev & NO_READER) != 0
     }
 
+    #[inline(always)]
     pub fn set_start_free(&self, ord: Ordering) -> bool {
         let prev = self.flags.fetch_or(START_FREE, ord);
         (prev & START_FREE) != 0
     }
 
+    #[inline(always)]
     pub fn clear_start_free(&self, ord: Ordering) -> bool {
         let prev = self.flags.fetch_and(!START_FREE, ord);
         (prev & START_FREE) != 0
@@ -64,22 +73,27 @@ impl AtomicSignal {
 }
 
 impl LoadedSignal {
+    #[inline(always)]
     pub fn has_action(&self) -> bool {
         self.flags != 0
     }
 
+    #[inline(always)]
     pub fn get_epoch(&self) -> bool {
         (self.flags & UPDATE_EPOCH) != 0
     }
 
+    #[inline(always)]
     pub fn get_reader(&self) -> bool {
         (self.flags & NO_READER) != 0
     }
 
+    #[inline(always)]
     pub fn get_writer(&self) -> bool {
         (self.flags & NO_WRITER) != 0
     }
 
+    #[inline(always)]
     pub fn start_free(&self) -> bool {
         (self.flags & START_FREE) != 0
     }
